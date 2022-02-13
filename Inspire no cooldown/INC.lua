c4eeb021bb756f4ed66631adaff239f9d4d48b5a
Jonathanbyersx <99598500+Jonathanbyersx@users.noreply.github.com>
2022-02-13 16:27:06 +0300

    Add files via upload

---
+++ Inspire no cooldown/INC.lua
@@ -0,0 +1,6 @@
+﻿--remove inspire aced cooldown
+local PlayerManager_has_enabled_cooldown_upgrade_original = PlayerManager.has_enabled_cooldown_upgrade
+function PlayerManager:has_enabled_cooldown_upgrade(category, upgrade, ...)
+	if category == "cooldown" and upgrade == "long_dis_revive" then return true end
+	return PlayerManager_has_enabled_cooldown_upgrade_original (self, category, upgrade, ...)
+end
